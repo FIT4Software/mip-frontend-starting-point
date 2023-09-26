e2760571f4421324cfa22ed2a1fd25c8f1247b2a
Juan Garcia <juan.garcia@aridosoftware.com>
2023-09-26 17:32:51 -0300

    title grid sp interfaces

--- src/starting-point/components/grid/TitleGrid.tsx
+++ src/starting-point/components/grid/TitleGrid.tsx
@@ -1,4 +1,4 @@
-import { ITitleGridProps } from '@interfaces';
+import { ITitleGridProps } from '@sp-interfaces';
 
 const TitleGrid = ({ children }: ITitleGridProps) => {
   return <h4 className="uppercase text-primary tracking-wide">{children}</h4>;
