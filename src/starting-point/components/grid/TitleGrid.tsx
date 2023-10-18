4edcc1f192a1a6ee5ccc41c8aef636ff1c8acbb7
dariofrison <Dario.Frison@aridosoftware.com>
2023-10-18 15:33:25 -0300

    changing_styles

--- src/starting-point/components/grid/TitleGrid.tsx
+++ src/starting-point/components/grid/TitleGrid.tsx
@@ -1,7 +1,7 @@
 import { ITitleGridProps } from '@sp-interfaces';
 
 const TitleGrid = ({ children }: ITitleGridProps) => {
-  return <h4 className="uppercase text-primary tracking-wide">{children}</h4>;
+  return <h4 className="uppercase font-semibold text-primary ">{children}</h4>;
 };
 
 export default TitleGrid;
